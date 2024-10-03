ebb0c6ff527ee8968b2c8618f6864f7ac23f3a77
ofhatwa <tofhatwa@gmail.com>
2024-10-03 22:46:56 +0200

    Updated the pascal's triangle code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,20 +3,21 @@
 This module is a Pascal's Triangle
 """
 
+
 def pascal_triangle(n):
     """
     It Returns a list of lists of integers of nth Pascal's triangle.
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
-    
+
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
