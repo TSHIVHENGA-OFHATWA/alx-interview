4842f46615bc5e23abc968c36be388eec12fbc62
ofhatwa <tofhatwa@gmail.com>
2024-10-17 19:40:28 +0200

    Adding the minoperatons file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module defines the function minOperations that calculates 
+the minimum number of operations needed to result in exactly
+n H characters in the file
+"""
+
+
+def minOperations(n):
+    """
+    Calculates the minimum number of operations needed to result to result in
+    exactly n H characters in the file
+
+    Args:
+        n (int): The target number of H characters.
+
+    Returns:
+        int: The minimum number of operations required, or 0 if n is < 2.
+    """
+    if n < 2:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
