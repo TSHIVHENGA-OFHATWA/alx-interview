85be8d5d71b4ca64a3f74e18dc5da5f0afddfb58
ofhatwa <tofhatwa@gmail.com>
2025-01-07 22:00:31 +0200

    Adding the code for the nqueen that is challenge of placing N non-attacking queens on an N×N chessboard

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import sys
+
+def is_safe(board, row, col, N):
+    for i in range(row):
+        if board[i][1] == col or \
+           board[i][1] - i == col - row or \
+           board[i][1] + i == col + row:
+            return False
+    return True
+
+def solve(board, row, N):
+    if row == N:
+        print(board)
+        return
+    
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row] = [row, col]
+            solve(board, row + 1, N)
+            board[row] = [-1, -1]
+
+def nqueens(N):
+    if not isinstance(N, int):
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[-1, -1] for _ in range(N)]
+    solve(board, 0, N)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    
+    nqueens(N)
