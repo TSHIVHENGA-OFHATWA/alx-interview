8787d81a479e360ec5fbe5003e20c8f9aa6d6b6e
ofhatwa <tofhatwa@gmail.com>
2025-01-26 20:07:15 +0200

    Adding the making change file that contain the code that return the fewest number of coins needed to meet total or -1 if total cannot be met

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given total.
+
+    Args:
+        coins (list): A list of the values of the coins in your possession.
+        total (int): The total amount to meet.
+
+    Returns:
+        int: Fewest number of coins needed to meet total or -1 if cannot.
+    """
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+
+    count = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        count += total // coin
+        total %= coin
+
+    return count if total == 0 else -1
